f55c6d1bf3776055c6467ab4b09e9c21700d1cc0
rogger2 <roggerhenrique043@gmail.com>
2016-06-08 18:22:46 -0300

    att

--- plugin.video.roggerstream/_Edit.py
+++
@@ -1,4 +0,0 @@
-import xbmcaddon
-
-MainBase = 'https://cld.pt/dl/download/6b8d6732-50ee-4f58-aef9-3a54ac88706e/ListaPrincipal.xml'
-#MainBase1 = 'https://cld.pt/dl/download/2c41d123-2876-4829-8a18-0c14d742e32a/play.html'
